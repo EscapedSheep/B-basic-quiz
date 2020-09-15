999950e0836a980ae7c9453a0aed913ed9514f81
EscapedSheep <sfgmdf001@126.com>
2020-09-15 13:24:39 +0800

    [maida] feat: add user existed check for get education api

--- src/main/java/com/thoughtworks/gtb/basic/quiz/service/EducationService.java
+++ src/main/java/com/thoughtworks/gtb/basic/quiz/service/EducationService.java
@@ -1,23 +1,34 @@
 package com.thoughtworks.gtb.basic.quiz.service;
 
 import com.thoughtworks.gtb.basic.quiz.domain.Education;
+import com.thoughtworks.gtb.basic.quiz.domain.User;
+import com.thoughtworks.gtb.basic.quiz.exception.UserNotFoundException;
 import com.thoughtworks.gtb.basic.quiz.repository.EducationRepository;
+import com.thoughtworks.gtb.basic.quiz.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EducationService {
     private EducationRepository educationRepository;
 
+    private UserRepository userRepository;
+
     @Autowired
-    public EducationService(EducationRepository educationRepository) {
+    public EducationService(EducationRepository educationRepository, UserRepository userRepository) {
         this.educationRepository = educationRepository;
+        this.userRepository = userRepository;
     }
 
     public List<Education> getEducation(long userId) {
-        return educationRepository.getEducations(userId);
+        Optional<User> findUser = userRepository.getUser(userId);
+        if (findUser.isPresent()) {
+            return educationRepository.getEducations(userId);
+        }
+        throw new UserNotFoundException();
     }
 
     public Education addEducation(long userId, Education education) {
